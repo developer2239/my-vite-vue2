66fa8697413e4fb533f2b51cf8fb99d3167daef4
developer2239 <42111134+developer2239@users.noreply.github.com>
2023-11-03 20:39:52 +0900

    create Header.vue and Footer.vue

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer">
+    <small>&copy;my-vite-todo</small>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  margin-top: 16px;
+  height: 30px;
+}
+</style>
